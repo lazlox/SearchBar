46f42585c01ee471a1c8e37f26ffe6f5ed78c15c
SzpakKamil <dev@kamilszpak.com>
2025-06-17 22:21:01 +0200

    FFA Section with new iOS 26 images

---
+++ Sources/SearchBar/SearchBar.docc/Resources/tutorials/first-filtering-app/code/FFAContentView-4-1.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Searcher
+//
+//  Created by KamilSzpak on 04/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    ...
+    var body: some View {
+        let groups = Dictionary(grouping: movies, by: \.genre)
+        let keys = groups.keys.sorted()
+        NavigationStack{
+                List{
+                    ForEach(keys){key in
+                        Section(key.name) {
+                            ForEach(groups[key] ?? []){
+                                movieRow(for: $0)
+                            }
+                        }
+                    }
+                }
+            .navigationTitle("Searcher")
+        }
+    }
+    ...
+}
+
+#Preview {
+    ContentView()
+}
